f6820dbb24a5e333701ecbfc56207ccfec3d7f78
Rong Sen Ng <wes.ngrongsen@gmail.com>
2021-07-24 17:07:45 +0800

    test: update tests for utc-date

--- src/__tests__/utc-date/error.spec.ts
+++ src/__tests__/utc-date/error.spec.ts
@@ -1,40 +1,51 @@
-import { utcDate } from '../../utc-date/index.js';
+import { test } from 'uvu';
+import * as assert from 'uvu/assert';
 
-it(`throws when first argument is 'null'`, async () => {
+import { utcDate } from '../../utc-date/index';
+
+test(`throws when first argument is 'null'`, async () => {
   try {
     await utcDate(null as never);
   } catch (e) {
-    expect(e.name).toStrictEqual('TypeError');
-    expect([
+    assert.is(e.name, 'TypeError');
+    assert.ok([
       `Cannot destructure property \`startDate\` of 'undefined' or 'null'.`,
       `Cannot destructure property 'startDate' of '(intermediate value)(intermediate value)(intermediate value)' as it is null.`,
-    ].some(n => e.message === n)).toStrictEqual(true);
+    ].some(n => e.message === n));
   }
 });
 
-it(`throws when invalid 'year'`, async () => {
+test(`throws when invalid 'year'`, async () => {
   try {
     await utcDate({ offset: { year: NaN } });
   } catch (e) {
-    expect(e).toStrictEqual(
-      new TypeError(`Expected 'year' to be a valid number, but received '${NaN}'`));
+    assert.equal(
+      e,
+      new TypeError(`Expected 'year' to be a valid number, but received '${NaN}'`)
+    );
   }
 });
 
-it(`throws when invalid 'month'`, async () => {
+test(`throws when invalid 'month'`, async () => {
   try {
     await utcDate({ offset: { month: NaN } });
   } catch (e) {
-    expect(e).toStrictEqual(
-      new TypeError(`Expected 'month' to be a valid number, but received '${NaN}'`));
+    assert.equal(
+      e,
+      new TypeError(`Expected 'month' to be a valid number, but received '${NaN}'`)
+    );
   }
 });
 
-it(`throws when invalid 'day'`, async () => {
+test(`throws when invalid 'day'`, async () => {
   try {
     await utcDate({ offset: { day: NaN } });
   } catch (e) {
-    expect(e).toStrictEqual(
-      new TypeError(`Expected 'day' to be a valid number, but received '${NaN}'`));
+    assert.equal(
+      e,
+      new TypeError(`Expected 'day' to be a valid number, but received '${NaN}'`)
+    );
   }
 });
+
+test.run();
--- src/__tests__/utc-date/utc-date.spec.ts
+++ src/__tests__/utc-date/utc-date.spec.ts
@@ -1,28 +1,33 @@
-import { utcDate } from '../../utc-date/index.js';
+import { test } from 'uvu';
+import * as assert from 'uvu/assert';
 
-it('returns UTC date with any arguments', async () => {
+import { utcDate } from '../../utc-date/index';
+
+test('returns UTC date with any arguments', async () => {
   const d = await utcDate();
 
-  expect(d).toStrictEqual(new Date(new Date().toJSON().replace(/^(.+)T.+/i, '$1')));
+  assert.equal(d, new Date(new Date().toJSON().replace(/^(.+)T.+/i, '$1')));
 });
 
-it(`returns UTC date with defined 'startDate'`, async () => {
+test(`returns UTC date with defined 'startDate'`, async () => {
   const d = await utcDate({ startDate: '2018-03-03' });
 
-  expect(d).toStrictEqual(new Date(new Date('2018-03-03').toJSON()));
+  assert.equal(d, new Date(new Date('2018-03-03').toJSON()));
 });
 
-it(`returns UTC date with defined 'offset'`, async () => {
+test(`returns UTC date with defined 'offset'`, async () => {
   const dated = new Date();
   const exp = new Date(Date.UTC(
     dated.getUTCFullYear() + 1, dated.getUTCMonth() + 2, dated.getUTCDate()));
   const d = await utcDate({ offset: { year: 1, month: 2 } });
 
-  expect(d).toStrictEqual(exp);
+  assert.equal(d, exp);
 });
 
-it(`returns UTC date with defined 'startDate' and 'offset'`, async () => {
+test(`returns UTC date with defined 'startDate' and 'offset'`, async () => {
   const d = await utcDate({ startDate: '2018-03-03', offset: { year: 1, month: 2 } });
 
-  expect(d).toStrictEqual(new Date(new Date('2019-05-03').toJSON()));
+  assert.equal(d, new Date(new Date('2019-05-03').toJSON()));
 });
+
+test.run();
