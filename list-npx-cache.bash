04b4ab7036b5d4e1b77fdc7a002138e2da98b069
Rong Sen Ng (motss) <wes.ngrongsen@gmail.com>
2022-02-05 16:29:03 +0800

    ci: debug

--- list-npx-cache.bash
+++ list-npx-cache.bash
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-files=$(find ~/.npm/_npx -type f | grep -v 'node_modules' | grep -v 'package-lock.json')
+# files=$(find ~/.npm/_npx -type f | grep -v 'node_modules' | grep -v 'package-lock.json')
+files=$(find ~/.npm/_npx -type f | grep -v 'node_modules')
 
 for a in $files
 do
