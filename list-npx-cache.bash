a9f18348483b1b6391928de1c8d1e516a52f8385
Rong Sen Ng (motss) <wes.ngrongsen@gmail.com>
2022-02-05 16:49:26 +0800

    ci: debug

--- list-npx-cache.bash
+++ list-npx-cache.bash
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-# files=$(find ~/.npm/_npx -type f | grep -v 'node_modules' | grep -v 'package-lock.json')
-files=$(find $(npm config get cache)/_npx -type f | grep -v 'node_modules')
+# NOTE: THere is a discrepancy in the npx cache in npm between 8.4.1 and 8.1.4.
+# There is an additional zx found in the npx cache when compared to the local npx cache using 8.1.4.
+
+files=$(find ~/.npm/_npx -type f | grep -v 'node_modules' | grep -v 'package-lock.json')
+# files=$(find $(npm config get cache)/_npx -type f | grep -v 'node_modules')
 
 for a in $files
 do
