c9febc11f180b290d42f7d054caa6e17a775f280
Rong Sen Ng (motss) <wes.ngrongsen@gmail.com>
2022-02-05 23:35:33 +0800

    ci: improve bash script

--- list-npx-cache.bash
+++ list-npx-cache.bash
@@ -3,16 +3,26 @@
 # NOTE: THere is a discrepancy in the npx cache in npm between 8.4.1 and 8.1.4.
 # There is an additional zx found in the npx cache when compared to the local npx cache using 8.1.4.
 
-files=$(find $(npm config get cache)/_npx -type f | grep -v 'node_modules' | grep -v 'package-lock.json')
-# files=$(find $(npm config get cache)/_npx -type f | grep -v 'node_modules')
+NPX_CACHE_DIR="$(npm config get cache)/_npx"
 
-for a in $files
-do
-  echo "::group::$a"
-  cat $a
+if [[ -d $NPX_CACHE_DIR ]]; then
+  FILES=$(find $NPX_CACHE_DIR -type f | grep -v 'node_modules' | grep -v 'package-lock.json')
+  # files=$(find $(npm config get cache)/_npx -type f | grep -v 'node_modules')
+
+  echo '[INFO] Listing all files in the npx cache...'
   echo $'\n'
-  echo "::endgroup::"
-done
+
+  for a in $FILES
+  do
+    echo "::group::$a"
+    cat $a
+    echo $'\n'
+    echo "::endgroup::"
+  done
+else
+  echo '[INFO] npx cache not found!'
+  echo $'\n'
+fi
 
 # NOTE: List all files with human readable size in the npm cache
 # echo "::group::$(ls -lhR $(npm config get cache) || echo 'nil')"
